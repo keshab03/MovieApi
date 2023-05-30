136e1b03f1efe7395b5eed35a91ab91766065fe5
Keshab Mahanta <94249491+keshab03@users.noreply.github.com>
2023-05-30 16:47:50 +0530

    Update Home.jsx

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
   const [characters, setCharacters] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [favorites, setFavorites] = useState([]);
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
 
   useEffect(() => {
     fetchCharacters();
@@ -35,6 +36,12 @@ const Home = () => {
     }
   };
 
+  const handleDetails = (name) => {
+    // Find the selected character from the characters array
+    const selected = characters.find((character) => character.name === name);
+    setSelectedCharacter(selected);
+  };
+
   return (
     <div>
       <h1>List</h1>
@@ -50,7 +57,9 @@ const Home = () => {
           {characters.map((character, index) => (
             <tr key={character.name}>
               <td>{index + 1}</td>
-              <td>{character.name}</td>
+              <td style={{cursor:'pointer'}} onClick={() => handleDetails(character.name)}>
+                {character.name}
+              </td>
               <td>
                 <button id='fav' onClick={() => handleFavorite(character.name)}>
                   <i className="fa-sharp fa-solid fa-star"></i>
@@ -60,15 +69,29 @@ const Home = () => {
           ))}
         </tbody>
       </table>
+      {selectedCharacter && (
+        <div id='details'>
+          <h2>Selected Character Details:</h2>
+          <p>Name: {selectedCharacter.name}</p>
+          <p>Height: {selectedCharacter.height}</p>
+          <p>Mass: {selectedCharacter.mass}</p>
+          <p>Hair Color: {selectedCharacter.hair_color}</p>
+          <p>Skin Color: {selectedCharacter.skin_color}</p>
+          <p>Eye Color: {selectedCharacter.eye_color}</p>
+          <p>Birth Year: {selectedCharacter.birth_year}</p>
+          <p>Gender: {selectedCharacter.gender}</p>
+        </div>
+      )}
       <div id='pagebtn'>
         <button
           disabled={currentPage === 1}
-          onClick={() => handlePageChange(currentPage - 1)}>
-          <i class="fa-sharp fa-solid fa-backward" style={{ color: 'black' }}></i>
+          onClick={() => handlePageChange(currentPage - 1)}
+        >
+          <i className="fa-sharp fa-solid fa-backward" style={{ color: 'black' }}></i>
         </button>
         {currentPage}
         <button onClick={() => handlePageChange(currentPage + 1)}>
-          <i class="fa-sharp fa-solid fa-forward"></i>
+          <i className="fa-sharp fa-solid fa-forward"></i>
         </button>
       </div>
     </div>
